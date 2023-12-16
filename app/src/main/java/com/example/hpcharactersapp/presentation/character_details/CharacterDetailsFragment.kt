f90bd9ad24c4150638e8a06bab748ed699316ae0
Limiel104 <a.myjak1995@gmail.com>
2023-12-16 13:45:07 +0100

    Character details screen ui

--- app/src/main/java/com/example/hpcharactersapp/presentation/character_details/CharacterDetailsFragment.kt
+++ app/src/main/java/com/example/hpcharactersapp/presentation/character_details/CharacterDetailsFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import coil.load
+import com.example.hpcharactersapp.R
 import com.example.hpcharactersapp.databinding.FragmentCharacterDetailsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -50,6 +52,53 @@ class CharacterDetailsFragment : Fragment() {
     fun displayCharacter() {
         viewModel.character.observe(viewLifecycleOwner) { character ->
             Log.i("TAG",character.toString())
+
+            binding.characterName.text = character.name
+            binding.characterImage.load(character.imageUrl)
+
+            if(character.alternateNames.isNotEmpty()) {
+                binding.characterAlternateNames.text = character.alternateNames.joinToString(", ")
+            }
+            else {
+                binding.characterAlternateNames.visibility = View.GONE
+            }
+
+            binding.basicInfoExpandIcon.load(R.drawable.ic_expand_more)
+            binding.characterSpecies.text = character.species
+            binding.characterGender.text = character.gender
+            binding.characterDateOfBirth.text = character.dateOfBirth
+            binding.characterAncestry.text = character.ancestry
+            binding.characterEyeColor.text = character.eyeColour
+            binding.characterHairColor.text = character.hairColour
+
+            if(character.alive) {
+                binding.characterAliveStatus.setText(R.string.alive)
+            }
+            else {
+                binding.characterAliveStatus.setText(R.string.dead)
+            }
+
+            binding.magicalCharacteristicsExpandIcon.load(R.drawable.ic_expand_more)
+            binding.characterPatronus.text = character.patronus
+            binding.characterWandCore.text = character.wand.core
+            binding.characterWandLength.text = character.wand.length.toString()
+            binding.characterWandWood.text = character.wand.wood
+
+            binding.affiliationExpandIcon.load(R.drawable.ic_expand_more)
+            binding.characterHouse.text = character.house
+
+            if(character.hogwartsStaff) {
+                binding.characterHStuff.setText(R.string.stuff)
+                binding.characterHStudent.visibility = View.INVISIBLE
+            }
+            else if(character.hogwartsStudent) {
+                binding.characterHStudent.setText(R.string.student)
+                binding.characterHStuff.visibility = View.INVISIBLE
+            }
+            else {
+                binding.characterHStudent.visibility = View.GONE
+                binding.characterHStuff.visibility = View.GONE
+            }
         }
     }
 }
