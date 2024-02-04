260f9462def44c311ede753cc4477b9203b6ecff
Limiel104 <a.myjak1995@gmail.com>
2024-02-04 14:01:38 +0100

    Ui tests for recycler view

--- app/src/androidTest/java/com/example/hpcharactersapp/domain/SuggestionRepositoryTestImpl.kt
+++ app/src/androidTest/java/com/example/hpcharactersapp/domain/SuggestionRepositoryTestImpl.kt
@@ -3,13 +3,12 @@ package com.example.hpcharactersapp.domain
 import com.example.hpcharactersapp.data.repository.SuggestionRepository
 import com.example.hpcharactersapp.domain.util.Resource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class SuggestionRepositoryTestImpl: SuggestionRepository {
-    override suspend fun addSuggestion(suggestion: String) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun addSuggestion(suggestion: String) {}
 
     override suspend fun getSuggestions(): Flow<Resource<List<String>>> {
-        TODO("Not yet implemented")
+        return flow { emit(Resource.Success(emptyList())) }
     }
 }
--- app/src/androidTest/java/com/example/hpcharactersapp/presentation/character_list/CharacterListFragmentTest.kt
+++ app/src/androidTest/java/com/example/hpcharactersapp/presentation/character_list/CharacterListFragmentTest.kt
@@ -4,6 +4,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.Visibility
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
@@ -12,14 +13,30 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.google.android.material.R.id.open_search_view_edit_text
 import com.example.hpcharactersapp.R
+import com.example.hpcharactersapp.data.repository.CharacterRepository
+import com.example.hpcharactersapp.data.repository.SuggestionRepository
+import com.example.hpcharactersapp.domain.CharacterRepositoryTestImpl
+import com.example.hpcharactersapp.domain.SuggestionRepositoryTestImpl
+import com.example.hpcharactersapp.domain.use_case.AddSuggestionUseCase
+import com.example.hpcharactersapp.domain.use_case.GetCharacterUseCase
+import com.example.hpcharactersapp.domain.use_case.GetCharactersUseCase
+import com.example.hpcharactersapp.domain.use_case.GetSuggestionsUseCase
 import com.example.hpcharactersapp.launchFragmentInHiltContainer
+import com.example.hpcharactersapp.presentation.di.AppModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Singleton
 
+@UninstallModules(AppModule::class)
 @HiltAndroidTest
 @OptIn(ExperimentalCoroutinesApi::class)
 class CharacterListFragmentTest {
@@ -34,33 +51,33 @@ class CharacterListFragmentTest {
 
     @Test
     fun test_isCharacterListFragmentLaunchedInHiltContainer() {
-        launchFragmentInHiltContainer<CharacterListFragment> {  }
+        launchFragmentInHiltContainer<CharacterListFragment> {}
     }
 
     @Test
     fun test_isCharacterListFragmentLaunched() {
-        launchFragmentInHiltContainer<CharacterListFragment> {  }
+        launchFragmentInHiltContainer<CharacterListFragment> {}
 
         onView(withId(R.id.character_list_fragment)).check(matches(isDisplayed()))
     }
 
     @Test
     fun test_isCharacterListFragmentDisplayedCorrectly() {
-        launchFragmentInHiltContainer<CharacterListFragment> {  }
+        launchFragmentInHiltContainer<CharacterListFragment> {}
 
         onView(withId(R.id.nestedScrollView)).check(matches(isDisplayed()))
         onView(withId(R.id.characterListRV)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
         onView(withId(R.id.appBarLayout)).check(matches(isDisplayed()))
         onView(withId(R.id.searchBar)).check(matches(isDisplayed()))
         onView(withHint(R.string.search_dots)).check(matches(isDisplayed()))
-        onView(withId(R.id.progressIndicator)).check(matches( isDisplayed()))
+        onView(withId(R.id.progressIndicator)).check(matches(withEffectiveVisibility(Visibility.INVISIBLE)))
         onView(withId(R.id.searchView)).check(matches(isDisplayed()))
         onView(withHint(R.string.search)).check(matches(withEffectiveVisibility(Visibility.GONE)))
     }
 
     @Test
     fun test_searchViewHintIsDisplayedAfterClick() {
-        launchFragmentInHiltContainer<CharacterListFragment> {  }
+        launchFragmentInHiltContainer<CharacterListFragment> {}
 
         onView(withHint(R.string.search)).check(matches(withEffectiveVisibility(Visibility.GONE)))
         onView(withId(R.id.searchBar)).perform(click())
@@ -69,7 +86,7 @@ class CharacterListFragmentTest {
 
     @Test
     fun test_searchViewContainsInsertedText() {
-        launchFragmentInHiltContainer<CharacterListFragment> {  }
+        launchFragmentInHiltContainer<CharacterListFragment> {}
 
         onView(withHint(R.string.search)).check(matches(withEffectiveVisibility(Visibility.GONE)))
         onView(withId(R.id.searchBar)).perform(click())
@@ -77,5 +94,63 @@ class CharacterListFragmentTest {
         onView(withId(open_search_view_edit_text)).perform(typeText("new text"))
         onView(withText("new text")).check(matches(isDisplayed()))
         onView(withId(open_search_view_edit_text)).check(matches(withText("new text")))
+    }
+
+    @Test
+    fun test_isCharacterListRVDisplayedOnLaunch() {
+        launchFragmentInHiltContainer<CharacterListFragment> {}
+
+        onView(withId(R.id.characterListRV)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_isCharacterListRVDisplayedCorrectly() {
+        launchFragmentInHiltContainer<CharacterListFragment> {}
+
+        onView(withId(R.id.characterListRV)).check(matches(isDisplayed()))
+
+        onView(withText("Harry Potter")).check(matches(isDisplayed()))
+        onView(withText("Hermione Granger")).check(matches(isDisplayed()))
+    }
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object TestAppModule {
+
+        @Provides
+        @Singleton
+        fun provideCharacterRepository(): CharacterRepository {
+            return CharacterRepositoryTestImpl()
+        }
+
+        @Provides
+        @Singleton
+        fun provideSuggestionRepository(): SuggestionRepository {
+            return SuggestionRepositoryTestImpl()
+        }
+
+        @Provides
+        @Singleton
+        fun provideGetCharactersUseCase(repository: CharacterRepository): GetCharactersUseCase {
+            return GetCharactersUseCase(repository)
+        }
+
+        @Provides
+        @Singleton
+        fun provideGetCharacterUseCase(repository: CharacterRepository): GetCharacterUseCase {
+            return GetCharacterUseCase(repository)
+        }
+
+        @Provides
+        @Singleton
+        fun provideGetSuggestionsUseCase(repository: SuggestionRepository): GetSuggestionsUseCase {
+            return GetSuggestionsUseCase(repository)
+        }
+
+        @Provides
+        @Singleton
+        fun provideAddSuggestionUseCase(repository: SuggestionRepository): AddSuggestionUseCase {
+            return AddSuggestionUseCase(repository)
+        }
     }
 }
